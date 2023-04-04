d9198c16378fe213e860737058b20ff203e1c1a4
Caner Duzenli <122194561+canerduzenli@users.noreply.github.com>
2023-04-04 17:30:18 -0500

    updatejs

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,40 +11,14 @@ const correctCheckSound = new Audio('./assets/sounds/mixkit-.wav');
 const incorrectCheckSound = new Audio('./assets/sounds/failure-drum-.mp3');
 const endSound = new Audio('./assets/sounds/mixkit-animated-small-group-applause-523.wav');
 const words = [
-  'algorithm', 'application', 'API', 'architecture', 'array', 'backend', 'binary',
-  'bug', 'cache', 'callback', 'class', 'client', 'cloud', 'code', 'compiler',
-  'component', 'compression', 'computer', 'conditional', 'constructor', 'CSS',
-  'database', 'debug', 'deployment', 'design', 'developer', 'DevOps', 'documentation',
-  'domain', 'encryption', 'endpoint', 'exception', 'framework', 'frontend', 'function',
-  'Git', 'HTML', 'HTTP', 'IDE', 'inheritance', 'interface', 'Internet', 'iteration',
-  'Java', 'JavaScript', 'JSON', 'library', 'Linux', 'loop', 'machine', 'memory',
-  'method', 'mobile', 'module', 'network', 'node', 'object', 'open-source', 'operating',
-  'platform', 'pointer', 'polymorphism', 'protocol', 'Python', 'query', 'recursion',
-  'repository', 'responsive', 'REST', 'runtime', 'SaaS', 'scalability', 'SDK',
-  'security', 'server', 'software', 'SQL', 'stack', 'storage', 'string', 'structure',
-  'syntax', 'system', 'testing', 'UI', 'UX', 'validation', 'variable', 'version',
-  'virtual', 'web', 'website', 'XML'
+  'ocean', 'mountain', 'forest', 'desert', 'beach', 'island', 'river',
+  'animal', 'bird', 'fish', 'insect', 'reptile', 'mammal', 'flora',
+  'climate', 'weather', 'hurricane', 'typhoon', 'tornado', 'earthquake', 'tsunami',
+  'geography', 'history', 'culture', 'language', 'music', 'art', 'literature',
+  'philosophy', 'religion', 'politics', 'economics', 'sociology', 'psychology',
+  'astronomy', 'physics', 'chemistry', 'biology', 'medicine', 'sports', 'fitness',
+  'nutrition', 'cooking', 'travel', 'tourism', 'adventure', 'leisure', 'entertainment'
 ];
-
-class Score {
-  constructor(date, hits, percentage) {
-    this._date = date;
-    this._hits = hits;
-    this._percentage = percentage;
-  }
-
-  get date() {
-    return this._date;
-  }
-
-  get hits() {
-    return this._hits;
-  }
-
-  get percentage() {
-    return this._percentage;
-  }
-}
 
 let timer;
 let timeLeft;
@@ -57,11 +31,10 @@ inputField.addEventListener("input", checkInput);
 volumeControl.addEventListener("input", adjustVolume);
 restartBtn.addEventListener('click', startGame);
 
-// volume settings
 function adjustVolume() {
   backgroundSound.volume = volumeControl.value;
 }
-// score 
+
 function updateScore() {
   score++;
   scoreDisplay.innerHTML = `Score: ${score}`;
@@ -70,7 +43,6 @@ function updateScore() {
 function startGame() {
   wordsTemp = [...words];
   backgroundSound.play();
-  correctCheckSound.play();
   startBtn.disabled = true;
   inputField.disabled = false;
   inputField.focus();
@@ -79,9 +51,9 @@ function startGame() {
   score = 0;
   scoreDisplay.innerHTML = `Score: ${score}`;
   showNextWord();
-  timer = setInterval(countdown, 1000);
-  resultContainer.classList.add('hidden');
+  timer = setInterval(countdown, 100);
 }
+
 function countdown() {
   timeLeft--;
   timerElement.textContent = `Time left: ${timeLeft}`;
@@ -92,7 +64,6 @@ function countdown() {
   }
 }
 
-// clean inputfield quikly when you write incorrect word
 inputField.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     if (inputField.value.trim().toUpperCase() === wordDisplay.innerHTML.toUpperCase()) {
@@ -139,48 +110,53 @@ function checkInput() {
   }
 }
 
-function restartGame() {
-  // Stop the timer if it is running
-  clearInterval(timer);
-
-  // Reset the game state
-  wordsTemp = [...words];
-  timeLeft = 99;
-  score = 0;
-  currentWord = null;
-  inputField.value = '';
-  inputField.disabled = true;
-  wordDisplay.textContent = '';
-  ScoreDisplay.textContent = '';
-  timerElement.textContent = '';
-  resultContainer.classList.add('hidden');
-
-  // Reset the UI
-  startBtn.disabled = false;
-  inputField.classList.remove('incorrect');
-  showResult(false);
+function saveScores(scoresArray) {
+  localStorage.setItem('gameScores', JSON.stringify(scoresArray));
 }
 
-// End function
+function loadScores() {
+  return JSON.parse(localStorage.getItem('gameScores')) || [];
+}
+
 function endGame() {
   startBtn.disabled = false;
   inputField.disabled = true;
   inputField.value = "";
   const percentage = (score / words.length) * 100;
-  const gameScore = new Score(new Date(), score, percentage);
-  console.log(gameScore);
-  backgroundSound.pause();
-  backgroundSound.currentTime = 0;
+  const gameScore = {
+    date: new Date(),
+    hits: score,
+    percentage: percentage
+  };
+
+  let scoresArray = loadScores();
+  scoresArray.push(gameScore);
+  scoresArray.sort((a, b) => b.hits - a.hits);
+  scoresArray.splice(50);
+  saveScores(scoresArray);
+
+  const topScores = scoresArray.slice(0, 9);
+  console.log("Top 9 scores:", topScores);
+
+  resultContainer.innerHTML = '';
   const scoreElement = document.createElement('p');
   scoreElement.textContent = `Score: ${score}`;
   const percentageElement = document.createElement('p');
   percentageElement.textContent = `Percentage: ${percentage.toFixed(2)}%`;
   const dateElement = document.createElement('p');
   dateElement.textContent = `Date: ${gameScore.date.toLocaleString()}`;
-  resultContainer.innerHTML = '';
   resultContainer.appendChild(scoreElement);
   resultContainer.appendChild(percentageElement);
   resultContainer.appendChild(dateElement);
+
+  const topScoresList = document.createElement('ol');
+  topScores.forEach(score => {
+    const listItem = document.createElement('li');
+    listItem.textContent = `Score: ${score.hits}, Percentage: ${score.percentage.toFixed(2)}%, Date: ${new Date(score.date).toLocaleString()}`;
+    topScoresList.appendChild(listItem);
+  });
+  resultContainer.appendChild(topScoresList);
+
   resultContainer.classList.remove('hidden');
   endSound.play();
 }
